7a4a851e223cf3f1740bf4436d8362cbc165b472
craig <craig@oppkey.com>
2021-09-20 16:36:39 -0700

    added image mode option

--- bin/tpreview.dart
+++ bin/tpreview.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 import 'package:args/command_runner.dart';
 import 'package:tpreview/basic_test.dart';
+import 'package:tpreview/image_mode_cli.dart';
 import 'package:tpreview/info.dart';
 import 'package:tpreview/save_frames_cli.dart';
 import 'package:tpreview/set_option_cli.dart';
 import 'package:tpreview/state.dart';
 import 'package:tpreview/take_picture.dart';
-import 'package:tpreview/video_mode.dart';
+import 'package:tpreview/video_mode_cli.dart';
 
 void main(List<String> arguments) async {
   final runner = CommandRunner('tpreview', 'RICOH THETA Live Preview tester')
@@ -16,7 +17,8 @@ void main(List<String> arguments) async {
     ..addCommand(SaveFrames())
     ..addCommand(VideoMode())
     ..addCommand(SetOption())
-    ..addCommand(BasicTest());
+    // ..addCommand(BasicTest())
+    ..addCommand(ImageMode());
 
   await runner.run(arguments).catchError((error) {
     if (error is! UsageException) throw error;
